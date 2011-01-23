ebf10c0e8e7adb059f2e2402b73fb49e0504b8e6
Jesper Josefsson <jesper.josefsson@gmail.com>
2011-01-23 20:27:01 +0100

    lade till variabler i train

--- Train.java
+++ Train.java
@@ -5,6 +5,22 @@
  */
  
  public class Train implements Runnable {
+   
+   // Denotes whether the train in question is traveling down or up the track
+   private boolean goingDown;
+   
+   // The speed with which the train travels
+   private int speed;
+   
+   // The id of the train
+   private int id;
+   
+   public Train(int id, int speed) {
+     this.id = id;
+     this.speed = speed;
+     
+   }
+   
    public void run() {
      
    }
